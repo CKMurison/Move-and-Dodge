f44760aa4d02a02c8e3d5cf609f49365068a2107
Cameron Murison <cmuris4@gmail.com>
2023-08-08 13:52:24 +0100

    added stop controls for front, back, left, right.

--- Scripts/ball_head.gd
+++ Scripts/ball_head.gd
@@ -6,12 +6,18 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
+	
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = 5
-	if Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -5
-	if Input.is_action_pressed("ui_up"):
+		
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -5
-	if Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down"):
 		velocity.z = 5
 	move_and_slide()
